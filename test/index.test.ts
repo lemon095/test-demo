f7f0c86c48ebbe8872d69590257889ad42a0ac85
lemon095 <lemonwork095@gmail.com>
2024-10-16 16:09:05 +0100

    feat: isDuoBaoPending 处理

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,29 @@
 import { test, expect } from "bun:test";
 import BaseSlot from "..";
+const slot = new BaseSlot();
 test("is not win over 15", () => {
-  expect(new BaseSlot().isNotWinOver15(-1)).toBeFalsy();
-  expect(new BaseSlot().isNotWinOver15(0)).toBeFalsy();
-  expect(new BaseSlot().isNotWinOver15(1)).toBeFalsy();
-  expect(new BaseSlot().isNotWinOver15(15)).toBeTrue();
-  expect(new BaseSlot().isNotWinOver15(16)).toBeTrue();
+	expect(slot.isNotWinOver15(-1)).toBeFalsy();
+	expect(slot.isNotWinOver15(0)).toBeFalsy();
+	expect(slot.isNotWinOver15(1)).toBeFalsy();
+	expect(slot.isNotWinOver15(15)).toBeTrue();
+	expect(slot.isNotWinOver15(16)).toBeTrue();
 });
 test("is pre win", () => {
-  expect(new BaseSlot().isPreWin({})).toBeFalse();
-  expect(new BaseSlot().isPreWin(null)).toBeFalse();
-  expect(new BaseSlot().isPreWin()).toBeFalse();
-  expect(new BaseSlot().isPreWin({ 1: [1, 2, 3] })).toBeTrue();
+	expect(slot.isPreWin({})).toBeFalse();
+	expect(slot.isPreWin(null)).toBe(false);
+	expect(slot.isPreWin()).toBeFalse();
+	expect(slot.isPreWin({ 1: [1, 2, 3] })).toBeTrue();
 });
 test("is DuoBao pending", () => {
-  expect(new BaseSlot().isDuoBaoPending({})).toBeFalse();
+	expect(slot.isDuoBaoPending({})).toBeFalse();
+	expect(slot.isDuoBaoPending({ preFs: { s: 0, ts: 10 } })).toBeFalse();
+	expect(slot.isDuoBaoPending({ preFs: { s: null } })).toBeUndefined();
+	expect(slot.isDuoBaoPending({ preFs: { s: 1, ts: 10 } })).toBeTrue();
+	expect(slot.isDuoBaoPending({ preFs: { s: 10, ts: 10 } })).toBeTrue();
+	expect(
+		slot.isDuoBaoPending({ preFs: { s: 2, ts: 10 }, preWp: { 1: [1, 2, 3] } })
+	).toBeTrue();
+	expect(
+		slot.isDuoBaoPending({ preFs: { s: 10, ts: 10 }, preWp: { 1: [1, 2, 3] } })
+	).toBeFalse();
 });

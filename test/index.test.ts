d9296ae39a2b6906e018ba2e480aa6a0a16a943a
lemon095 <lemonwork095@gmail.com>
2024-10-17 14:24:13 +0100

    feat: 单元测试使用 TDD 规则

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,69 +1,107 @@
-import { test, expect } from "bun:test";
+import { test, expect, describe, it } from "bun:test";
 import BaseSlot from "..";
 const slot = new BaseSlot();
-test("is not win over 15", () => {
-	expect(slot.isNotWinOver15(-1)).toBeFalsy();
-	expect(slot.isNotWinOver15(0)).toBeFalsy();
-	expect(slot.isNotWinOver15(1)).toBeFalsy();
-	expect(slot.isNotWinOver15(15)).toBeTrue();
-	expect(slot.isNotWinOver15(16)).toBeTrue();
+describe("is not win over 15", () => {
+	it("input -1, output false", () => {
+		expect(slot.isNotWinOver15(-1)).toBeFalsy();
+	});
+	it("input 0 or 1, output false", () => {
+		expect(slot.isNotWinOver15(0)).toBeFalsy();
+		expect(slot.isNotWinOver15(1)).toBeFalsy();
+	});
+	it("input 15 or 16, output true", () => {
+		expect(slot.isNotWinOver15(15)).toBeTrue();
+		expect(slot.isNotWinOver15(16)).toBeTrue();
+	});
 });
-test("is pre win", () => {
-	expect(slot.isPreWin({})).toBeFalse();
-	expect(slot.isPreWin(null)).toBe(false);
-	expect(slot.isPreWin()).toBeFalse();
-	expect(slot.isPreWin({ 1: [1, 2, 3] })).toBeTrue();
+
+describe("is pre win", () => {
+	it("input empty value, output false", () => {
+		expect(slot.isPreWin({})).toBeFalse();
+		expect(slot.isPreWin(null)).toBe(false);
+		expect(slot.isPreWin()).toBeFalse();
+	});
+	it("input pre wp, output true", () => {
+		expect(slot.isPreWin({ 1: [1, 2, 3] })).toBeTrue();
+	});
 });
-test("is DuoBao pending", () => {
-	expect(slot.isDuoBaoPending({})).toBeFalse();
-	expect(slot.isDuoBaoPending({ preFs: { s: 0, ts: 10 } })).toBeFalse();
-	expect(slot.isDuoBaoPending({ preFs: { s: null } })).toBeUndefined();
-	expect(slot.isDuoBaoPending({ preFs: { s: 1, ts: 10 } })).toBeTrue();
-	expect(slot.isDuoBaoPending({ preFs: { s: 10, ts: 10 } })).toBeTrue();
-	expect(
-		slot.isDuoBaoPending({ preFs: { s: 2, ts: 10 }, preWp: { 1: [1, 2, 3] } })
-	).toBeTrue();
-	expect(
-		slot.isDuoBaoPending({ preFs: { s: 10, ts: 10 }, preWp: { 1: [1, 2, 3] } })
-	).toBeFalse();
+describe("is DuoBao pending", () => {
+	it("输入空对象输出 false", () => {
+		expect(slot.isDuoBaoPending({})).toBeFalse();
+	});
+	it("fs.s=0&&fs.ts=10 输出 false", () => {
+		expect(slot.isDuoBaoPending({ preFs: { s: 0, ts: 10 } })).toBeFalse();
+	});
+	it("fs.s=null 输出 undefined", () => {
+		expect(slot.isDuoBaoPending({ preFs: { s: null } })).toBeUndefined();
+	});
+	it("fs.s>0&&上一次未中奖, 输出 true", () => {
+		expect(slot.isDuoBaoPending({ preFs: { s: 1, ts: 10 } })).toBeTrue();
+	});
+	it("fs.s===fs.ts 输出 true", () => {
+		expect(slot.isDuoBaoPending({ preFs: { s: 10, ts: 10 } })).toBeTrue();
+	});
+	it("fs.s >0 && fs.s !== ts && 上一次中奖 输出 true", () => {
+		expect(
+			slot.isDuoBaoPending({ preFs: { s: 2, ts: 10 }, preWp: { 1: [1, 2, 3] } })
+		).toBeTrue();
+	});
+	it("fs.s === fs.s && 上一次未中奖 输出 false", () => {
+		expect(
+			slot.isDuoBaoPending({
+				preFs: { s: 10, ts: 10 },
+				preWp: { 1: [1, 2, 3] },
+			})
+		).toBeFalse();
+	});
 });
-test("random tgm", () => {
-	expect(slot.getRandomTgmByIcon([2])).toBe(2);
-	expect(slot.getRandomTgmByIcon([3, 3])).toBe(3);
+describe("random tgm", () => {
+	it("随机长度为 1", () => {
+		expect(slot.getRandomTgmByIcon([2])).toBe(2);
+	});
+	it("随机长度大于 1", () => {
+		expect(slot.getRandomTgmByIcon([3, 3])).toBe(3);
+	});
 });
-test("tmd computed", () => {
-	expect(
-		slot.getTmd({
-			icons: [6, 8, 2, 6],
-			preTmd: [[3, 2]],
-			twp: { 9: [0] },
-			trns: [6],
-			tgmByIcon: 2,
-			tgmWeight: [2, 3],
-		})
-	).toEqual([[2, 2]]);
-	expect(
-		slot.getTmd({
-			icons: [6, 8, 2, 6],
-			preTmd: [[2, 2]],
-			twp: { 9: [3] },
-			trns: [6],
-			tgmByIcon: 2,
-			tgmWeight: [2, 3],
-		})
-	).toEqual([[2, 2]]);
-	expect(
-		slot.getTmd({
-			icons: [6, 2, 4, 6],
-			preTmd: [[1, 2]],
-			twp: { 9: [2, 3] },
-			trns: [6, 3],
-			tgmByIcon: 2,
-			tgmWeight: [2, 2],
-		})
-	).toEqual([
-		[1, 2],
-		[2, 2],
-	]);
+describe("tmd computed", () => {
+	it("上一次中奖且中奖位置小于倍数图标的位置，本次掉落未出现 2", () => {
+		expect(
+			slot.getTmd({
+				icons: [6, 8, 2, 6],
+				preTmd: [[3, 2]],
+				twp: { 9: [0] },
+				trns: [6],
+				tgmByIcon: 2,
+				tgmWeight: [2, 3],
+			})
+		).toEqual([[2, 2]]);
+	});
+	it("上一次中奖且中奖位置大于倍数图标的位置，本次掉落未出现 2", () => {
+		expect(
+			slot.getTmd({
+				icons: [6, 8, 2, 6],
+				preTmd: [[2, 2]],
+				twp: { 9: [3] },
+				trns: [6],
+				tgmByIcon: 2,
+				tgmWeight: [2, 3],
+			})
+		).toEqual([[2, 2]]);
+	});
+	it("上一次中奖且中奖位置大于倍数图标的位置，本次掉落出现 2", () => {
+		expect(
+			slot.getTmd({
+				icons: [6, 2, 4, 6],
+				preTmd: [[1, 2]],
+				twp: { 9: [2, 3] },
+				trns: [2, 3],
+				tgmByIcon: 2,
+				tgmWeight: [2, 2],
+			})
+		).toEqual([
+			[1, 2],
+			[2, 2],
+		]);
+	});
 	// 测试用例处理
 });

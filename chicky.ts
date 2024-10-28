1040cc217c2806e230118132510e4e79c3470bfb
lemon095 <lemonwork095@gmail.com>
2024-10-29 00:06:12 +0800

    feat: 暂存

--- chicky.ts
+++ chicky.ts
@@ -24,37 +24,36 @@ export enum UserType {
 
 /** 用户操作 */
 export enum GameOperate {
-	/** 小鸡在左侧 */
-	left = 1,
-	/** 小鸡在右侧 */
-	right = 2,
-	/** 开始游戏 */
-	start_play = 3,
-	/** 游戏胜利/领奖 */
-	winner_paly = 4,
+  /** 小鸡在左侧 */
+  left = 1,
+  /** 小鸡在右侧 */
+  right = 2,
+  /** 开始游戏 */
+  start_play = 3,
+  /** 游戏胜利/领奖 */
+  winner_paly = 4,
 }
 
 /** 车（物体）的位置 */
 export enum CarPos {
-	/** 车在左侧 */
-	left = 2,
-	/** 车在右侧 */
-	right = 1,
+  /** 车在左侧 */
+  left = 2,
+  /** 车在右侧 */
+  right = 1,
 }
 
 export interface BaseChickyParams {
-	/** 用户行为 */
-	ps: GameOperate;
-	/** 金币模式信息 */
-	ib: boolean;
-	/** 投注额 */
-	cs: number;
-	/** 投注线 */
-	ml: number;
+  /** 用户行为 */
+  ps: GameOperate;
+  /** 金币模式信息 */
+  ib: boolean;
+  /** 投注额 */
+  cs: number;
+  /** 投注线 */
+  ml: number;
 }
 
 export default class BaseChicky {
-<<<<<<< HEAD
   /**
    * @ib true: 金币模式; false: 普通模式
    * @ps 表示状态 1,2表示操作阶段，3表示继续/开始游戏，4表示领取
@@ -102,13 +101,7 @@ export default class BaseChicky {
    * @gcc 本次金币的数量
    * @preAgcc 上一次金币的数量
    */
-  public agcc({
-    gcc,
-    preAgcc,
-  }: {
-    gcc: number;
-    preAgcc: number;
-  }): number {
+  public agcc({ gcc, preAgcc }: { gcc: number; preAgcc: number }): number {
     if (!this.ib || this.ps === 3) {
       return 0;
     }
@@ -120,88 +113,87 @@ export default class BaseChicky {
    * agcv 收集的金币金额
    * @agcc 金币的总数量
    */
-  public agcv(agcc:number): number {
-    const agcv =  new Decimal(agcc).mul(this.totalBet).mul(0.5).toNumber()
+  public agcv(agcc: number): number {
+    const agcv = new Decimal(agcc).mul(this.totalBet).mul(0.5).toNumber();
     return agcv;
   }
 
+  public readonly ps: GameOperate;
+  public readonly ib: boolean;
+  public readonly cs: number;
+  public readonly ml: number;
+  public readonly totalBet: number;
 
-	public readonly ps: GameOperate;
-	public readonly ib: boolean;
-	public readonly cs: number;
-	public readonly ml: number;
-	public readonly totalBet: number;
+  /**
+   * base Chicky 构造器
+   * @param {Object} options - 配置选项
+   * @param {GameOperate} options.ps 必填，用户的操作信息
+   * @param {boolean} options.ib 必填，是否为金币模式
+   * @param {boolean} options.cs 必填，投注额
+   * @param {boolean} options.ml 必填，投注线
+   */
+  constructor({ ps, ib, cs, ml }: BaseChickyParams) {
+    this.ps = ps;
+    this.ib = ib;
+    this.cs = cs;
+    this.ml = ml;
+    this.totalBet = new Decimal(cs).mul(ml).toNumber();
+  }
 
-	/**
-	 * base Chicky 构造器
-	 * @param {Object} options - 配置选项
-	 * @param {GameOperate} options.ps 必填，用户的操作信息
-	 * @param {boolean} options.ib 必填，是否为金币模式
-	 * @param {boolean} options.cs 必填，投注额
-	 * @param {boolean} options.ml 必填，投注线
-	 */
-	constructor({ ps, ib, cs, ml }: BaseChickyParams) {
-		this.ps = ps;
-		this.ib = ib;
-		this.cs = cs;
-		this.ml = ml;
-		this.totalBet = new Decimal(cs).mul(ml).toNumber();
-	}
+  /**
+   * 是否为金币模式
+   * @returns {boolean} true:金币模式，false:非金币模式
+   */
+  public get isGlod(): boolean {
+    return this.ib;
+  }
 
-	/**
-	 * 是否为金币模式
-	 * @returns {boolean} true:金币模式，false:非金币模式
-	 */
-	public get isGlod(): boolean {
-		return this.ib;
-	}
+  public testFn() {
+    const result = this.convertWeights([
+      { icon: 0, weight: 22 },
+      { icon: 1, weight: 59 },
+      { icon: 2, weight: 18 },
+      { icon: 3, weight: 1 },
+    ]);
+    console.log(result);
+    return "test";
+  }
 
-	public testFn() {
-		const result = this.convertWeights([
-			{ icon: 0, weight: 22 },
-			{ icon: 1, weight: 59 },
-			{ icon: 2, weight: 18 },
-			{ icon: 3, weight: 1 },
-		]);
-		console.log(result);
-		return "test";
-	}
+  /**
+   * 上一局是否赢
+   * @returns {boolean} true:赢，false:输
+   */
+  public get isPrevWin(): boolean {
+    return this.isCurrentWin(CarPos.left, GameOperate.left);
+  }
 
-	/**
-	 * 上一局是否赢
-	 * @returns {boolean} true:赢，false:输
-	 */
-	public get isPrevWin(): boolean {
-		return this.isCurrentWin(CarPos.left, GameOperate.left);
-	}
-
-	/**
-	 * 本局是否输赢
-	 * @param {CarPos} 车的位置
-	 * @param {GameOperate} 小鸡的位置，选填。不填则为当局游戏的小鸡位置
-	 * @returns {boolean} true:赢，false:输
-	 */
-	public isCurrentWin(carPos: CarPos, ps = this.ps) {
-		if (ps === GameOperate.left && carPos === CarPos.left) return true;
-		if (ps === GameOperate.right && carPos === CarPos.right) return true;
-		return false;
-	}
-	/**
-	 * 随机车的位置
-	 * @returns {CarPos} 左二右一
-	 */
-	public getRR(): CarPos {
-		const r = random.int(CarPos.left, CarPos.right);
-		return r;
-	}
-	/**
-	 * 将权重配置转为权重表
-	 * @param {PGSlot.WeightCfg[]} weights - 权重配置信息
-	 * @returns 权重表数据
-	 */
-	public convertWeights(weights: PGSlot.WeightCfg[]): number[] {
-		return flatMapDeep(
-			weights.map((item) => Array(item.weight).fill(item.icon))
-		);
-	}
+  /**
+   * 本局是否输赢
+   * @param {CarPos} 车的位置
+   * @param {GameOperate} 小鸡的位置，选填。不填则为当局游戏的小鸡位置
+   * @returns {boolean} true:赢，false:输
+   */
+  public isCurrentWin(carPos: CarPos, ps = this.ps) {
+    if (ps === GameOperate.left && carPos === CarPos.left) return true;
+    if (ps === GameOperate.right && carPos === CarPos.right) return true;
+    return false;
+  }
+  /**
+   * 随机车的位置
+   * @returns {CarPos} 左二右一
+   */
+  public getRR(): CarPos {
+    const r = random.int(CarPos.left, CarPos.right);
+    return r;
+  }
+  /**
+   * 将权重配置转为权重表
+   * @param {PGSlot.WeightCfg[]} weights - 权重配置信息
+   * @returns 权重表数据
+   */
+  public convertWeights(weights: PGSlot.WeightCfg[]): number[] {
+    return flatMapDeep(
+      weights.map((item) => Array(item.weight).fill(item.icon))
+    );
+  }
 }
--- test/chicky.test.ts
+++ test/chicky.test.ts
@@ -1,42 +1,37 @@
 import { expect, describe, it, beforeAll, test } from "bun:test";
 import BaseChicky, { UserType } from "../chicky";
 import { chunk } from "lodash";
-<<<<<<< HEAD
-const slot = new BaseChicky();
-
-=======
 const slot = new BaseChicky({ cs: 1, ib: true, ml: 1, ps: 3 });
->>>>>>> a5ea43f1736b82543e4b1cac02172b0d20979e2b
 describe("chicky test", () => {
   it("test", () => {
     expect(slot.testFn()).toBe("test");
   });
 });
 describe("随机车的位置: getRR", () => {
-	it("验证是否为 1 或者 2", () => {
-		expect([1, 2]).toContain(slot.getRR());
-	});
-	it("多次循环验证是否未 1 或者 2", () => {
-		for (let i = 0; i < 100; i++) {
-			expect([1, 2]).toContain(slot.getRR());
-		}
-	});
-	const numTrials = 10000;
-	const counts = { 1: 0, 2: 0 };
-	const expectedProbability = 0.5; // 期望的概率是 0.5
-	const tolerance = 0.05; // 允许的误差范围
-	beforeAll(() => {
-		for (let i = 0; i < numTrials; i++) {
-			const result = slot.getRR();
-			counts[result]++;
-		}
-	});
-	it("验证 getRR 返回值的分布概率是否 1:1", () => {
-		const total = numTrials;
-		const probability1 = counts[1] / total;
-		const probability2 = counts[2] / total;
-		// 验证返回值的概率是否接近 0.5，允许一定的误差
-		expect(probability1).toBeCloseTo(expectedProbability, tolerance);
-		expect(probability2).toBeCloseTo(expectedProbability, tolerance);
-	});
+  it("验证是否为 1 或者 2", () => {
+    expect([1, 2]).toContain(slot.getRR());
+  });
+  it("多次循环验证是否未 1 或者 2", () => {
+    for (let i = 0; i < 100; i++) {
+      expect([1, 2]).toContain(slot.getRR());
+    }
+  });
+  const numTrials = 10000;
+  const counts = { 1: 0, 2: 0 };
+  const expectedProbability = 0.5; // 期望的概率是 0.5
+  const tolerance = 0.05; // 允许的误差范围
+  beforeAll(() => {
+    for (let i = 0; i < numTrials; i++) {
+      const result = slot.getRR();
+      counts[result]++;
+    }
+  });
+  it("验证 getRR 返回值的分布概率是否 1:1", () => {
+    const total = numTrials;
+    const probability1 = counts[1] / total;
+    const probability2 = counts[2] / total;
+    // 验证返回值的概率是否接近 0.5，允许一定的误差
+    expect(probability1).toBeCloseTo(expectedProbability, tolerance);
+    expect(probability2).toBeCloseTo(expectedProbability, tolerance);
+  });
 });

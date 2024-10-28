3799b019ea65a36f05b0758391e46abbd0a283e9
lemon095 <lemonwork095@gmail.com>
2024-10-28 23:10:30 +0800

    gcc 逻辑

--- chicky.ts
+++ chicky.ts
@@ -1,28 +1,66 @@
 import Decimal from "decimal.js";
 import {
-	flatMapDeep,
-	flatten,
-	isArray,
-	isEmpty,
-	isNumber,
-	isUndefined,
-	keys,
-	union,
-	values,
+  flatMapDeep,
+  flatten,
+  isArray,
+  isEmpty,
+  isNumber,
+  isUndefined,
+  keys,
+  union,
+  values,
 } from "lodash";
 import random from "random";
 
 export enum UserType {
-	/** 试玩 */
-	trail = 0,
-	/** 新用户 */
-	newuser = 1,
-	/** 正常用户 */
-	common = 2,
+  /** 试玩 */
+  trail = 0,
+  /** 新用户 */
+  newuser = 1,
+  /** 正常用户 */
+  common = 2,
 }
 
 export default class BaseChicky {
-	public testFn() {
-		return "test";
-	}
+  /**
+   * @ib true: 金币模式; false: 普通模式
+   * @ps 表示状态 1,2表示操作阶段，3表示继续/开始游戏，4表示领取
+   */
+  public gcc({
+    ib,
+    ps,
+    isWin,
+  }: {
+    ib: boolean;
+    ps: number;
+    isWin: boolean;
+  }): number {
+    if (!ib || ps === 3 || ps === 4 || !isWin) {
+      return 0;
+    }
+    //根据概率随机
+    const confProbability: Record<number, number> = {
+      0: 22,
+      1: 59,
+      2: 18,
+      3: 1,
+    };
+    let Data: Record<number, number> = {};
+    let totalProbability = 0;
+    for (const key in confProbability) {
+      totalProbability += confProbability[key];
+      Data[key] = totalProbability;
+    }
+    const randNum = Math.random() * totalProbability;
+    for (const key in Data) {
+      if (randNum < Data[key]) {
+        return key.toNumber();
+      }
+    }
+
+    return 1;
+  }
+  public testFn() {
+    return "test";
+  }
 }
--- test/chicky.test.ts
+++ test/chicky.test.ts
@@ -2,8 +2,9 @@ import { expect, describe, it } from "bun:test";
 import BaseChicky, { UserType } from "../chicky";
 import { chunk } from "lodash";
 const slot = new BaseChicky();
+
 describe("chicky test", () => {
-	it("test", () => {
-		expect(slot.testFn()).toBe("test");
-	});
+  it("test", () => {
+    expect(slot.testFn()).toBe("test");
+  });
 });

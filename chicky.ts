1de8f823730936357afa9fee7075e79926a343b4
lemon095 <lemonwork095@gmail.com>
2024-10-28 15:22:17 +0000

    feat: 修改逻辑

--- chicky.ts
+++ chicky.ts
@@ -23,6 +23,13 @@ export enum UserType {
 
 export default class BaseChicky {
 	public testFn() {
+		const result = this.convertWeights([
+			{ icon: 0, weight: 22 },
+			{ icon: 1, weight: 59 },
+			{ icon: 2, weight: 18 },
+			{ icon: 3, weight: 1 },
+		]);
+		console.log(result);
 		return "test";
 	}
 	/**
@@ -32,5 +39,15 @@ export default class BaseChicky {
 	public getRR(): 1 | 2 {
 		const r = random.int(1, 2);
 		return r as 1 | 2;
+	}
+	/**
+	 * 将权重配置转为权重表
+	 * @param {PGSlot.WeightCfg[]} weights - 权重配置信息
+	 * @returns 权重表数据
+	 */
+	public convertWeights(weights: PGSlot.WeightCfg[]): number[][] {
+		return flatMapDeep(
+			weights.map((item) => Array(item.weight).fill(item.icon))
+		);
 	}
 }

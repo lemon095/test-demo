bf0aae91dc896c42c2c9f2e364fa1de8cc290ac4
lemon095 <lemonwork095@gmail.com>
2024-10-29 22:34:13 +0800

    提交gcc

--- chicky.ts
+++ chicky.ts
@@ -55,44 +55,6 @@ export interface BaseChickyParams {
 }
 
 // export default class BaseChicky {
-//   /**
-//    * @ib true: 金币模式; false: 普通模式
-//    * @ps 表示状态 1,2表示操作阶段，3表示继续/开始游戏，4表示领取
-//    */
-//   public gcc({
-//     ib,
-//     ps,
-//     isWin,
-//   }: {
-//     ib: boolean;
-//     ps: number;
-//     isWin: boolean;
-//   }): number {
-//     if (!ib || ps === 3 || ps === 4 || !isWin) {
-//       return 0;
-//     }
-//     //根据概率随机
-//     const confProbability: Record<number, number> = {
-//       0: 22,
-//       1: 59,
-//       2: 18,
-//       3: 1,
-//     };
-//     let Data: Record<number, number> = {};
-//     let totalProbability = 0;
-//     for (const key in confProbability) {
-//       totalProbability += confProbability[key];
-//       Data[key] = totalProbability;
-//     }
-//     const randNum = random.int(1, 100);
-//     for (const key in Data) {
-//       if (randNum < Data[key]) {
-//         return Number(key);
-//       }
-//     }
-
-//     return 0;
-//   }
 // 	/** 用户行为 */
 // 	ps: GameOperate;
 // 	/** 金币模式信息 */
@@ -129,6 +91,24 @@ export default class BaseChicky {
     this.ml = ml;
     this.prevSi = prevSi;
     this.totalBet = new Decimal(cs).mul(ml).toNumber();
+  }
+  /**
+   * @ib true: 金币模式; false: 普通模式
+   * @ps 表示状态 1,2表示操作阶段，3表示继续/开始游戏，4表示领取
+   */
+  public gcc({ isWin }: { isWin: boolean }): number {
+    if (!this.ib || this.ps === 3 || this.ps === 4 || !isWin) {
+      return 0;
+    }
+    //根据概率随机
+    const randNum = random.int(1, 100);
+    const result = this.convertWeights([
+      { icon: 0, weight: 22 },
+      { icon: 1, weight: 59 },
+      { icon: 2, weight: 18 },
+      { icon: 3, weight: 1 },
+    ]);
+    return result[randNum];
   }
 
   /**

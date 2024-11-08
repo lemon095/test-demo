7fbd57e631165a8e415baceeb922066e96889bae
lemon095 <lemonwork095@gmail.com>
2024-11-08 15:38:50 +0000

    feat: 计算 mf

--- src/slot/feat.ts
+++ src/slot/feat.ts
@@ -1,22 +1,32 @@
 import random from "random";
 import BaseSlot, { type BaseSlotOptions } from ".";
+import { keys, toNumber, union, valuesIn } from "lodash";
 
 export default class ClassFeatSlot extends BaseSlot {
 	constructor(options: BaseSlotOptions) {
 		super(options);
 	}
-	/** 获取倍率相关信息 */
+	/**
+	 * 墨西哥狂欢：mf 金框倍率信息
+	 * @param {Object} options - 配置信息
+	 * @param {Array} options.gsp - 当前的金框信息
+	 * @param {Object} options.prevMf - 上一局的mf信息
+	 * @returns {Object} - 金框倍率信息
+	 */
 	public getMf({
 		gsp,
 		prevMf,
 	}: {
-		gsp: number[];
+		gsp?: number[];
 		prevMf?: Record<string, number>;
 	}): Record<string, number> {
-		return gsp.reduce((acc, key) => {
+		const prevPos = keys(prevMf).map(toNumber);
+		const currentPos = union(gsp, prevPos);
+		return currentPos.reduce((acc, key) => {
+			const gm = prevMf?.[key] ?? random.int(0, 20);
 			return {
 				...acc,
-				[key]: random.int(0, 20),
+				[key]: gm,
 			};
 		}, {} as Record<string, number>);
 	}
--- src/slot/test/mf.test.ts
+++ src/slot/test/mf.test.ts
@@ -10,10 +10,23 @@ const slot = new BaseSlot({
 	cs: 0,
 	ml: 0,
 });
-describe("墨西哥狂欢： mf金框倍率信息", () => {
-	it("获取随机倍率信息", () => {
-		const mf = slot.getMf({ gsp: [2, 5, 7] });
-		console.log("mf", mf);
-		expect(mf).toBeObject();
+describe("墨西哥: mf金框倍率信息", () => {
+	it("根据不同的入参判断返回值是否为 object", () => {
+		expect(slot.getMf({})).toBeObject();
+		expect(slot.getMf({ gsp: [2, 5, 7] })).toBeObject();
+		expect(slot.getMf({ prevMf: {} })).toBeObject();
+		expect(slot.getMf({ prevMf: { 2: 0, 5: 0, 7: 2 } })).toBeObject();
+	});
+	it("prev mf 和 gsp 索引相同，判断值是否为 prev mf 的值", () => {
+		expect(
+			slot.getMf({ prevMf: { 2: 0, 5: 0, 7: 2 }, gsp: [2, 5, 7] })
+		).toEqual({
+			2: 0,
+			5: 0,
+			7: 2,
+		});
+	});
+	it("不存在 prev mf, 判断当前的 mf 的倍率信息是否正确", () => {
+		expect(slot.getMf({ gsp: [2, 5, 7] }));
 	});
 });

760209d74687a1330a7f3cdede11c110f9b61cfa
lemon095 <lemonwork095@gmail.com>
2024-11-16 13:42:52 +0000

    feat: 修改构建逻辑

--- scripts/clearComments.ts
+++ scripts/clearComments.ts
@@ -4,24 +4,31 @@ import stripJsonComments from "strip-json-comments";
 
 function clearBaseSlot() {
 	// 读取带注释的文件
-	const inputFilePath = path.join(import.meta.dir, "../src/slot/index.ts");
-	const outputFilePath = path.join(import.meta.dir, "../src/slot/BaseSlot.ts");
-	readFile(inputFilePath, "utf8", (err, data) => {
-		if (err) {
-			console.error("Error reading file:", err);
-			return;
-		}
-
-		// 移除注释
-		const strippedData = stripJsonComments(data);
-
-		// 写入无注释的文件
-		writeFile(outputFilePath, strippedData, "utf8", (err) => {
+	const paths = ["../src/slot/index.ts", "../src/slot/RnsAdapter.ts"];
+	const output = [
+		"../src/slot/output/BaseSlot.ts",
+		"../src/slot/output/RnsAdapter.ts",
+	];
+	paths.forEach((_path, i) => {
+		const inputFilePath = path.join(import.meta.dir, _path);
+		const outputFilePath = path.join(import.meta.dir, output[i]);
+		readFile(inputFilePath, "utf8", (err, data) => {
 			if (err) {
-				console.error("Error writing file:", err);
+				console.error("Error reading file:", err);
 				return;
 			}
-			console.log("File has been written successfully.");
+
+			// 移除注释
+			const strippedData = stripJsonComments(data);
+
+			// 写入无注释的文件
+			writeFile(outputFilePath, strippedData, "utf8", (err) => {
+				if (err) {
+					console.error("Error writing file:", err);
+					return;
+				}
+				console.log("File has been written successfully.");
+			});
 		});
 	});
 }

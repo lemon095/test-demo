2d066acb2c6f70db0e34a4ab78a9773699c2b409
lemon095 <lemonwork095@gmail.com>
2025-02-11 20:07:58 +0000

    test: 单测逻辑处理

--- src/slot/test/gml.test.ts
+++ src/slot/test/gml.test.ts
@@ -127,17 +127,49 @@ describe("gml 倍率信息::蝶恋花", () => {
       expect(gml).toEqual([3, 6, 9, 30]);
     });
   });
-  describe("上一局已累加最终倍率", () => {
+  describe("上一局中奖", () => {
     it("rswl 存在三条数据", () => {
-      const gml = slot.getGml({
-        baseList: [1, 2, 3, 5],
-        highList: [3, 6, 9, 15],
-        prevGml: [1, 2, 3, 10],
-        isDuoBaoPending: false,
-        rswl: [[1, 4]],
-        baseGm: 5,
+      const slot = new BaseSlot({
+        rlWeights: RL_WEIGHTS,
+        trlWeights: TRL_WEIGHTS,
+        userType: UserType.common,
+        cs: 0.02,
+        ml: 1,
+        prevSi: {
+          wp: { 1: [] },
+        },
       });
-      expect(gml).toEqual([1, 2, 3, 15]);
+      expect(
+        slot.getGml({
+          baseList: [1, 2, 3, 5],
+          highList: [3, 6, 9, 15],
+          prevGml: slot.isPreWin ? [1, 2, 3, 10] : null,
+          isDuoBaoPending: false,
+          rswl: [[1, 4]],
+          baseGm: 5,
+        })
+      ).toEqual([1, 2, 3, 15]);
+    });
+  });
+  describe("上一局未中奖", () => {
+    it("rswl 存在三条数据", () => {
+      const slot = new BaseSlot({
+        rlWeights: RL_WEIGHTS,
+        trlWeights: TRL_WEIGHTS,
+        userType: UserType.common,
+        cs: 0.02,
+        ml: 1,
+      });
+      expect(
+        slot.getGml({
+          baseList: [1, 2, 3, 5],
+          highList: [3, 6, 9, 15],
+          prevGml: slot.isPreWin ? [1, 2, 3, 10] : null,
+          isDuoBaoPending: false,
+          rswl: [[1, 4]],
+          baseGm: 5,
+        })
+      ).toEqual([1, 2, 3, 10]);
     });
   });
 });

c255a1f4f42494f2d61c24e074b0a0039b99dadb
lemon095 <lemonwork095@gmail.com>
2025-03-05 00:23:48 +0000

    feat: 针对不同列长度的 rns 计算逻辑处理

--- src/slot/RnsAdapter.ts
+++ src/slot/RnsAdapter.ts
@@ -1,106 +1,129 @@
-import { chunk, difference, uniq } from "lodash";
+import { difference, isEmpty, uniq } from "lodash";
 import random from "random";
 import { RnsCalculateType } from "utils/helper";
 
 export interface RnsAdapterOptions {
-	/** 本局随机出来的 rl 数据 */
-	rl: number[][];
-	/** 上一次中奖的位置，通常代指 ptr、ptbr 等字段 */
-	prevWinPos: number[];
-	/** 每一列的长度 */
-	colLength: number;
-	/** 计算方式 */
-	mode: RnsCalculateType;
-	/** 上一局的rl数据 */
-	prevRl: number[];
-	/** 上一局的 trl 数据, 不传默认为空数组 */
-	prevTrl?: number[];
-	/** 强控不中奖需要用到的 图标 id 信息 */
-	iconIds: number[];
+  /** 本局随机出来的 rl 数据 */
+  rl: number[][];
+  /** 上一次中奖的位置，通常代指 ptr、ptbr 等字段 */
+  prevWinPos: number[];
+  /** 计算方式 */
+  mode: RnsCalculateType;
+  /** 上一局的rl数据 */
+  prevRl: number[];
+  /** 上一局的 trl 数据, 不传默认为空数组 */
+  prevTrl?: number[];
+  /** 强控不中奖需要用到的 图标 id 信息 */
+  iconIds: number[];
 }
 
 export default class RnsAdapter {
-	/** 本局 rns 数据 */
-	public rns: number[][] = [];
-	/** 所有列可以消费的图标 */
-	public allColByIcons: number[] = [];
-	/** 计算方式 */
-	private readonly mode: RnsCalculateType;
-	/** 新的 rl 数据 */
-	private readonly newRl: number[][];
+  /** 本局 rns 数据 */
+  public rns: number[][] = [];
+  /** 所有列可以消费的图标 */
+  public allColByIcons: number[] = [];
+  /** 计算方式 */
+  private readonly mode: RnsCalculateType;
+  /** 新的 rl 数据 */
+  private readonly newRl: number[][];
+  /** 所有列的起始位置和结束位置 */
+  private readonly columnsRange: number[][] = [];
 
-	constructor({ mode, rl, ...options }: RnsAdapterOptions) {
-		this.mode = mode;
-		this.newRl = rl;
-		this.rns = rl.map((_) => []);
-		this.adapterHandle(options);
-	}
+  constructor({ mode, rl, ...options }: RnsAdapterOptions) {
+    this.mode = mode;
+    this.newRl = rl;
+    this.rns = rl.map((_) => []);
+    const columnsLength = rl.map((_) => _.length);
+    this.columnsRange = columnsLength.map((colLength, colIndex) => {
+      if (colIndex === 0) {
+        return [0, colLength - 1];
+      }
+      const start = columnsLength.slice(0, colIndex).reduce((acc, cur) => {
+        return acc + cur;
+      }, 0);
+      return [start, start + colLength - 1];
+    });
+    this.adapterHandle(options);
+  }
 
-	private adapterHandle(options: Omit<RnsAdapterOptions, "mode" | "rl">) {
-		const adapterFn = {
-			[RnsCalculateType.RNS强控不中奖]: this.rnsNotWinner.bind(this),
-			[RnsCalculateType.RNS随机]: this.rnsRadom.bind(this),
-		}[this.mode];
-		return adapterFn(options);
-	}
+  private adapterHandle(options: Omit<RnsAdapterOptions, "mode" | "rl">) {
+    const adapterFn = {
+      [RnsCalculateType.RNS强控不中奖]: this.rnsNotWinner.bind(this),
+      [RnsCalculateType.RNS随机]: this.rnsRadom.bind(this),
+    }[this.mode];
+    return adapterFn(options);
+  }
 
-	private rnsNotWinner({
-		prevRl,
-		colLength,
-		prevWinPos,
-		prevTrl = [],
-		iconIds,
-	}: Omit<RnsAdapterOptions, "mode" | "rl">) {
-		const icons = iconIds;
-		const oldRl: number[][] = chunk(prevRl, colLength);
-		// 拿到 old rl 的第二列。先不过滤掉中奖的图标了
-		const preRlColumn2 = [...oldRl[1]];
-		// 奖 trl 的图标全部排出
-		preRlColumn2.push(...prevTrl);
-		// 拿到第一列可以随机的图标信息
-		const col1ByIcons = difference(icons, uniq(preRlColumn2));
-		// 获取第一列消失的图标信息
-		const delInColumn1 = prevWinPos.filter((idx) => idx < colLength);
-		// 随机新的图标，填充第一列
-		for (let row = 0; row < delInColumn1.length; row++) {
-			const idx = random.int(0, col1ByIcons.length - 1);
-			this.rns[0].push(col1ByIcons[idx]);
-		}
-		// 获取第一列未消失的图标
-		const notRemoveByCol1 = oldRl[0].filter(
-			(_, idx) => !delInColumn1.includes(idx)
-		);
-		// 拿到最新的 rl 第一列数据
-		const newRlColumn1 = [...this.rns[0], ...notRemoveByCol1];
-		// console.log("newRlColumn1", newRlColumn1);
-		// 拿到后续所有列都能随机的图标
-		const allColByIcons = difference(icons, newRlColumn1);
-		// 填充后续所有列的图标
-		prevWinPos.forEach((idx) => {
-			const delIndex = Math.floor(idx / colLength);
-			if (delIndex === 0) {
-				return;
-			}
-			const iconIdx = random.int(0, allColByIcons.length - 1);
-			this.rns[delIndex].push(allColByIcons[iconIdx]);
-		});
-		this.allColByIcons = allColByIcons;
-	}
+  private rnsNotWinner({
+    prevRl,
+    prevWinPos,
+    prevTrl = [],
+    iconIds,
+  }: Omit<RnsAdapterOptions, "mode" | "rl">) {
+    const icons = iconIds;
+    const oldRl: number[][] = prevRl.reduce((acc, icon, pos) => {
+      const colIndex = this.columnsRange.findIndex(
+        (range) => pos >= range[0] && pos <= range[1]
+      );
+      if (colIndex === -1) return acc;
+      if (isEmpty(acc[colIndex])) {
+        acc[colIndex] = [icon];
+      } else {
+        acc[colIndex].push(icon);
+      }
+      return acc;
+    }, [] as number[][]);
+    // 拿到 old rl 的第二列。先不过滤掉中奖的图标了
+    const preRlColumn2 = [...oldRl[1]];
+    // 将 trl 的图标全部排出
+    preRlColumn2.push(...prevTrl);
+    // 拿到第一列可以随机的图标信息
+    const col1ByIcons = difference(icons, uniq(preRlColumn2));
+    // 获取第一列消失的图标信息
+    const delInColumn1 = prevWinPos.filter(
+      (idx) => idx < this.columnsRange[1][0]
+    );
+    // 随机新的图标，填充第一列
+    for (let row = 0; row < delInColumn1.length; row++) {
+      const idx = random.int(0, col1ByIcons.length - 1);
+      this.rns[0].push(col1ByIcons[idx]);
+    }
+    // 获取第一列未消失的图标
+    const notRemoveByCol1 = oldRl[0].filter(
+      (_, idx) => !delInColumn1.includes(idx)
+    );
+    // 拿到最新的 rl 第一列数据
+    const newRlColumn1 = [...this.rns[0], ...notRemoveByCol1];
+    // console.log("newRlColumn1", newRlColumn1);
+    // 拿到后续所有列都能随机的图标
+    const allColByIcons = difference(icons, newRlColumn1);
+    // 填充后续所有列的图标
+    prevWinPos.forEach((pos) => {
+      const colIndex = this.columnsRange.findIndex(
+        (range) => pos >= range[0] && pos <= range[1]
+      );
+      if (colIndex === 0) {
+        return;
+      }
+      const iconIdx = random.int(0, allColByIcons.length - 1);
+      this.rns[colIndex].push(allColByIcons[iconIdx]);
+    });
+    this.allColByIcons = allColByIcons;
+  }
 
-	/**
-	 * rns 掉落纯随机逻辑
-	 * @param {object} options 配置参数
-	 * @param {number[]} options.prevWinPos 上一次中奖的位置，通常代指 ptr、ptbr 等字段
-	 * @param {number} options.colLength 每一列的长度
-	 * @param {number[][]} options.newRl 本局随机出来的 rl 数据
-	 */
-	private rnsRadom({
-		prevWinPos,
-		colLength,
-	}: Omit<RnsAdapterOptions, "mode" | "rl">) {
-		prevWinPos.forEach((idx) => {
-			const delIndex = Math.floor(idx / colLength);
-			this.rns[delIndex].push(this.newRl[delIndex][idx - delIndex * colLength]);
-		});
-	}
+  /**
+   * rns 掉落纯随机逻辑
+   * @param {object} options 配置参数
+   * @param {number[]} options.prevWinPos 上一次中奖的位置，通常代指 ptr、ptbr 等字段
+   * @param {number[][]} options.newRl 本局随机出来的 rl 数据
+   */
+  private rnsRadom({ prevWinPos }: Omit<RnsAdapterOptions, "mode" | "rl">) {
+    prevWinPos.forEach((pos) => {
+      const colIndex = this.columnsRange.findIndex(
+        (range) => pos >= range[0] && pos <= range[1]
+      );
+      const colRange = this.columnsRange[colIndex];
+      this.rns[colIndex].push(this.newRl[colIndex][pos - colRange[0]]);
+    });
+  }
 }
--- src/slot/test/rns.test.ts
+++ src/slot/test/rns.test.ts
@@ -5,208 +5,206 @@ import { RnsCalculateType, UserType } from "utils/helper";
 import { chunk, keys, values } from "lodash";
 
 describe("固定中奖路线: rns", () => {
-	it("修改", () => {
-		const slot = new BaseSlot({
-			rlWeights: RL_WEIGHTS,
-			trlWeights: TRL_WEIGHTS,
-			userType: UserType.common,
-			cs: 0,
-			ml: 0,
-		});
-		const rns = slot.getRns({
-			mode: RnsCalculateType.RNS随机,
-			rl: chunk(
-				[2, 7, 4, 4, 10, 0, 6, 6, 7, 7, 0, 5, 1, 5, 8, 3, 9, 1, 10, 5],
-				4
-			),
-			prevWinPos: [2],
-			colLength: 4,
-			prevRl: [7, 4, 9, 4, 10, 9, 6, 6, 7, 7, 9, 5, 1, 5, 8, 3, 9, 1, 10, 5],
-			iconIds: [2, 3, 4, 5, 6, 7, 8, 9, 10],
-		});
-		expect(rns.rns).toEqual([[4], [], [], [], []]);
-		expect(
-			slot.getMf({
-				gmTables: MF_WEIGHTS,
-				gsp: [2, 5, 7],
-				iconIds: [2, 3, 4, 5, 6],
-			})
-		).toBeObject();
-		expect(
-			slot.getMf({ gmTables: MF_WEIGHTS, prevMf: {}, iconIds: [2, 3, 4, 5, 6] })
-		).toBeObject();
-		expect(
-			slot.getMf({
-				gmTables: MF_WEIGHTS,
-				prevMf: { 2: 0, 5: 0, 7: 2 },
-				iconIds: [2, 3, 4, 5, 6],
-			})
-		).toBeObject();
-	});
+  it("修改", () => {
+    const slot = new BaseSlot({
+      rlWeights: RL_WEIGHTS,
+      trlWeights: TRL_WEIGHTS,
+      userType: UserType.common,
+      cs: 0,
+      ml: 0,
+    });
+    const rns = slot.getRns({
+      mode: RnsCalculateType.RNS随机,
+      rl: chunk(
+        [2, 7, 4, 4, 10, 0, 6, 6, 7, 7, 0, 5, 1, 5, 8, 3, 9, 1, 10, 5],
+        4
+      ),
+      prevWinPos: [2],
+      prevRl: [7, 4, 9, 4, 10, 9, 6, 6, 7, 7, 9, 5, 1, 5, 8, 3, 9, 1, 10, 5],
+      iconIds: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+    });
+    expect(rns.rns).toEqual([[4], [], [], [], []]);
+    expect(
+      slot.getMf({
+        gmTables: MF_WEIGHTS,
+        gsp: [2, 5, 7],
+        iconIds: [2, 3, 4, 5, 6],
+      })
+    ).toBeObject();
+    expect(
+      slot.getMf({ gmTables: MF_WEIGHTS, prevMf: {}, iconIds: [2, 3, 4, 5, 6] })
+    ).toBeObject();
+    expect(
+      slot.getMf({
+        gmTables: MF_WEIGHTS,
+        prevMf: { 2: 0, 5: 0, 7: 2 },
+        iconIds: [2, 3, 4, 5, 6],
+      })
+    ).toBeObject();
+  });
 });
 describe("相邻中奖路线: rns", () => {
-	const prevSi = {
-		rl: [
-			4, 8, 8, 1, 10, 4, 4, 11, 11, 12, 1, 10, 7, 4, 5, 10, 10, 10, 10, 10, 8,
-			8, 8, 6, 6, 9, 5, 4, 11, 10,
-		],
-		orl: [
-			4, 8, 8, 1, 10, 4, 4, 11, 11, 12, 1, 10, 7, 4, 5, 10, 10, 10, 10, 10, 8,
-			8, 8, 6, 6, 9, 5, 4, 11, 10,
-		],
-		trl: [5, 5, 10, 0],
-		torl: [5, 5, 10, 0],
-		ebb: {
-			"1": {
-				fp: 5,
-				lp: 6,
-				bt: 2,
-				ls: 1,
-			},
-			"2": {
-				fp: 15,
-				lp: 17,
-				bt: 2,
-				ls: 1,
-			},
-			"3": {
-				fp: 18,
-				lp: 19,
-				bt: 1,
-				ls: 2,
-			},
-			"4": {
-				fp: 20,
-				lp: 22,
-				bt: 2,
-				ls: 1,
-			},
-			"5": {
-				fp: 23,
-				lp: 24,
-				bt: 2,
-				ls: 1,
-			},
-		},
-		wp: {
-			"4": [0, 5, 6, 13],
-		},
-		twp: {
-			"4": [],
-		},
-		eb: {
-			"2": {
-				fp: 15,
-				lp: 17,
-				bt: 2,
-				ls: 1,
-			},
-			"3": {
-				fp: 18,
-				lp: 19,
-				bt: 1,
-				ls: 2,
-			},
-			"4": {
-				fp: 20,
-				lp: 22,
-				bt: 2,
-				ls: 1,
-			},
-			"5": {
-				fp: 23,
-				lp: 24,
-				bt: 2,
-				ls: 1,
-			},
-		},
-		esb: {
-			"1": [5, 6],
-			"2": [15, 16, 17],
-			"3": [18, 19],
-			"4": [20, 21, 22],
-			"5": [23, 24],
-		},
-		es: {
-			"2": [15, 16, 17],
-			"3": [18, 19],
-			"4": [20, 21, 22],
-			"5": [23, 24],
-		},
-		tptbr: [],
-		snww: {
-			"4": 1,
-		},
-		nowpr: [5, 5, 6, 3, 3, 5],
-		pcwc: 1,
-		fstc: null,
-		st: 1,
-		gt: null,
-		nst: 4,
-		cwc: 1,
-		lw: {
-			"4": 0.5,
-		},
-		tw: 0.5,
-		ctw: 0.5,
-		gwt: 1,
-		rwsp: {
-			"4": 25,
-		},
-		fs: null,
-		aw: 0.5,
-		tb: 0.4,
-		np: 0.1,
-		now: 6750,
-		bwp: {
-			"4": [[0], [5, 6], [13]],
-		},
-		ptbr: [0, 5, 6, 13],
-		rs: null,
-		ssaw: 0.5,
-		ge: [1, 11],
-		tbb: 0.4,
-		ml: 1,
-		cs: 0.02,
-		pf: 2,
-		wk: "0_C",
-		wt: "C",
-		hashr: "0:4;1;4#8;10;11#8;4;11#R#4#00122041#MV#0.4#MT#1#MG#0.5#",
-		psid: "498610810440569248",
-		sid: "498610810440569248",
-		bl: 9999994.1,
-		blab: 9999993.6,
-		blb: 9999994,
-		mr: null,
-		ocr: null,
-		wfg: null,
-		wbn: null,
-		pmt: null,
-		wid: 0,
-	};
-	it("上一次中奖", () => {
-		const slot = new BaseSlot({
-			rlWeights: RL_WEIGHTS,
-			trlWeights: TRL_WEIGHTS,
-			userType: UserType.common,
-			cs: 0,
-			ml: 0,
-		});
-		const rl = chunk(
-			[
-				9, 3, 11, 3, 2, 8, 8, 4, 10, 11, 3, 10, 6, 0, 2, 10, 12, 9, 12, 0, 6, 6,
-				6, 4, 4, 12, 5, 10, 8, 9,
-			],
-			5
-		);
-		const rns = slot.getRns({
-			rl,
-			prevRl: prevSi.rl,
-			prevWinPos: prevSi.ptbr || [],
-			colLength: rl[0].length,
-			iconIds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-			mode: RnsCalculateType.RNS随机,
-		});
-		console.log("rns", rns.rns);
-	});
+  const prevSi = {
+    rl: [
+      4, 8, 8, 1, 10, 4, 4, 11, 11, 12, 1, 10, 7, 4, 5, 10, 10, 10, 10, 10, 8,
+      8, 8, 6, 6, 9, 5, 4, 11, 10,
+    ],
+    orl: [
+      4, 8, 8, 1, 10, 4, 4, 11, 11, 12, 1, 10, 7, 4, 5, 10, 10, 10, 10, 10, 8,
+      8, 8, 6, 6, 9, 5, 4, 11, 10,
+    ],
+    trl: [5, 5, 10, 0],
+    torl: [5, 5, 10, 0],
+    ebb: {
+      "1": {
+        fp: 5,
+        lp: 6,
+        bt: 2,
+        ls: 1,
+      },
+      "2": {
+        fp: 15,
+        lp: 17,
+        bt: 2,
+        ls: 1,
+      },
+      "3": {
+        fp: 18,
+        lp: 19,
+        bt: 1,
+        ls: 2,
+      },
+      "4": {
+        fp: 20,
+        lp: 22,
+        bt: 2,
+        ls: 1,
+      },
+      "5": {
+        fp: 23,
+        lp: 24,
+        bt: 2,
+        ls: 1,
+      },
+    },
+    wp: {
+      "4": [0, 5, 6, 13],
+    },
+    twp: {
+      "4": [],
+    },
+    eb: {
+      "2": {
+        fp: 15,
+        lp: 17,
+        bt: 2,
+        ls: 1,
+      },
+      "3": {
+        fp: 18,
+        lp: 19,
+        bt: 1,
+        ls: 2,
+      },
+      "4": {
+        fp: 20,
+        lp: 22,
+        bt: 2,
+        ls: 1,
+      },
+      "5": {
+        fp: 23,
+        lp: 24,
+        bt: 2,
+        ls: 1,
+      },
+    },
+    esb: {
+      "1": [5, 6],
+      "2": [15, 16, 17],
+      "3": [18, 19],
+      "4": [20, 21, 22],
+      "5": [23, 24],
+    },
+    es: {
+      "2": [15, 16, 17],
+      "3": [18, 19],
+      "4": [20, 21, 22],
+      "5": [23, 24],
+    },
+    tptbr: [],
+    snww: {
+      "4": 1,
+    },
+    nowpr: [5, 5, 6, 3, 3, 5],
+    pcwc: 1,
+    fstc: null,
+    st: 1,
+    gt: null,
+    nst: 4,
+    cwc: 1,
+    lw: {
+      "4": 0.5,
+    },
+    tw: 0.5,
+    ctw: 0.5,
+    gwt: 1,
+    rwsp: {
+      "4": 25,
+    },
+    fs: null,
+    aw: 0.5,
+    tb: 0.4,
+    np: 0.1,
+    now: 6750,
+    bwp: {
+      "4": [[0], [5, 6], [13]],
+    },
+    ptbr: [0, 5, 6, 13],
+    rs: null,
+    ssaw: 0.5,
+    ge: [1, 11],
+    tbb: 0.4,
+    ml: 1,
+    cs: 0.02,
+    pf: 2,
+    wk: "0_C",
+    wt: "C",
+    hashr: "0:4;1;4#8;10;11#8;4;11#R#4#00122041#MV#0.4#MT#1#MG#0.5#",
+    psid: "498610810440569248",
+    sid: "498610810440569248",
+    bl: 9999994.1,
+    blab: 9999993.6,
+    blb: 9999994,
+    mr: null,
+    ocr: null,
+    wfg: null,
+    wbn: null,
+    pmt: null,
+    wid: 0,
+  };
+  it("上一次中奖", () => {
+    const slot = new BaseSlot({
+      rlWeights: RL_WEIGHTS,
+      trlWeights: TRL_WEIGHTS,
+      userType: UserType.common,
+      cs: 0,
+      ml: 0,
+    });
+    const rl = chunk(
+      [
+        9, 3, 11, 3, 2, 8, 8, 4, 10, 11, 3, 10, 6, 0, 2, 10, 12, 9, 12, 0, 6, 6,
+        6, 4, 4, 12, 5, 10, 8, 9,
+      ],
+      5
+    );
+    const rns = slot.getRns({
+      rl,
+      prevRl: prevSi.rl,
+      prevWinPos: prevSi.ptbr || [],
+      iconIds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      mode: RnsCalculateType.RNS随机,
+    });
+    console.log("rns", rns.rns);
+  });
 });
